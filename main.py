f9d916b3177ac644752646e91b643b41585f3bc4
plamistef <plamstef@icloud.com>
2020-05-30 16:19:06 +0200

    add delete password

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if (foundUser['username'] == username):
         q = [
             inquirer.List('commands',
                 message='What are you interested in?',
-                choices=['see all passwords', 'add new password','quit'],
+                choices=['see all passwords', 'add a new password','delete a password','quit'],
                 )]
 
         while(True):
@@ -47,7 +47,7 @@ if (foundUser['username'] == username):
             a = inquirer.prompt(q)
             if a['commands'] == 'see all passwords':
                 dal.get_all_passwords(con,foundUser['id'],password)
-            elif a['commands'] == 'add new password':
+            elif a['commands'] == 'add a new password':
                 site = inquirer.text(message="Enter the name of the website")
                 foundSite = dal.get_site(con,foundUser['id'],site)
                 if(foundSite != None):
@@ -56,9 +56,11 @@ if (foundUser['username'] == username):
                     user = inquirer.text(message="Enter username/email")
                     new = dal.add_password(con,site,user,foundUser['id'],password)
                     print("\n",new)
-            # elif a['commands'] == 'delete password':
-            #     to_delete = input("Enter password id: \n")
-            #     dal.delete_pass(con,to_delete)
+            elif a['commands'] == 'delete a password':
+                dal.get_all_passwords(con,foundUser['id'],password)
+                #to_delete = input("\nEnter #password id: \n")
+                to_delete = inquirer.text(message="Enter password id")
+                dal.delete_pass(con,to_delete)
             elif a['commands'] == 'quit':
                 break
     
